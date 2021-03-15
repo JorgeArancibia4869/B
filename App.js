0be379835b9497cbaed30565ab0f1f3b17b6758c
Leonardo Espinoza <espinoza.leonardo@gmail.com>
2021-03-15 13:37:50 -0300

    Refactoriza Linking a otra clase

--- App.js
+++ App.js
@@ -3,60 +3,85 @@ import { View, Text, StyleSheet, Button } from 'react-native';
 import * as Linking from 'expo-linking';
 
 
-const getParamsUrl = (link) => {
-  console.log(link)
-    //Para recuperar la ruta y los parámetros
-    const reDeepLinkURL = /^(\w+)\:\/\/([^\?]*)(\?(.*))?$/;
-    const matches = reDeepLinkURL.exec(link);
-    const { schema, path, queryParamsStr } = { schema: matches[1], path: '/' + matches[2], queryParamsStr: matches[4] };
-    let queryParams = {};
-    if (queryParamsStr.length > 0) {
-      queryParamsStr.split('&').forEach(pairStr => {
-        const pair = pairStr.split('=');
-        queryParams[pair[0]] = pair[1];
-      })
-    }
-    //const { path, queryParams } = Linking.parse(link)
-    console.log("path: ",path)
-    console.log("queryParams: ", queryParams)
-    return { schema, path, queryParams }
+const urlFromAlicePath2 = 'wptbobpr://path2';
+const urlFromAlice = 'wptbobpr://';
+console.log(`urlsFromAlice: "${urlFromAlice}" y "${urlFromAlicePath2}"`);
+
+class DeepLinkingTrigger {
+  constructor(schema) {
+    this.schema = schema
+  }
+
+  parseURL = (url) => {
+    console.log(url)
+      //Para recuperar la ruta y los parámetros
+      const reDeepLinkURL = /^(\w+)\:\/\/([^\?]*)(\?(.*))?$/;
+      const matches = reDeepLinkURL.exec(url);
+      if (matches === null) {
+        return { error: new Error(`Url inválida: ${url}`), url };
+      }
+      const { schema, path, queryParamsStr } = { schema: matches[1], path: '/' + matches[2], queryParamsStr: matches[4] };
+      // Convertir el querystring en objeto queryParams
+      let queryParams = {};
+      if (queryParamsStr.length > 0) {
+        queryParamsStr.split('&').forEach(pairStr => {
+          const pair = pairStr.split('=');
+          queryParams[pair[0]] = pair[1];
+        })
+      }
+      console.log("parseURL::(schema, path, queryParams): ", schema, path, queryParams)
+      // Validaciones finales
+      let wasTriggered = false;
+      if (scheme != this.schema) {
+        return { error: new Error('Schema no corresponde al de esta aplicación.'), wasTriggered, scheme, path, queryParams, url };
+      }
+      if (path != '/') { wasTriggered = true; }
+      if (queryParamsStr != '') { wasTriggered = true; }
+      return { error: null, wasTriggered, schema, path, queryParams, url };
+  }
+
+  async getInitialURL() {
+    const url = await Linking.getInitialURL();
+    const { error, wasTriggered, scheme, path, queryParams, url } = this.parseAndAnalize(url);
+    if (error) { return Promise.reject(error); }
+    return Promise.resolve({ wasTriggered, scheme, path, queryParams, url, wasInitial: true });
+  }
+
+  getEventURL() {
+    return new Promise((resolve, reject) => {
+      Linking.addEventListener('url', ( {url} ) => {
+        const { error, wasTriggered, scheme, path, queryParams, url } = this.parseAndAnalize(url);
+        if (error) { return reject(error); }
+        resolve({ wasTriggered, scheme, path, queryParams, url, wasInitial: false });
+      });
+    });
+  }
 }
 
-export default function App() {
-  const urlFromAlicePath2 = 'wptbobpr://path2'; //Linking.createURL('path2', {}, 'wptbobpr')
-  const urlFromAlice = 'wptbobpr://'; //Linking.createURL('', {}, 'wptbobpr')
-  console.log(`urlsFromAlice: "${urlFromAlice}" y "${urlFromAlicePath2}"`);
+const dlTrigger = new DeepLinkingTrigger('wptbobpr');
 
+export default function App() {
   const [link, setLink] = React.useState('')
   const [path, setPath] = React.useState('')
   const [queryParams, setQueryParams] = React.useState({})
   const [outputs, setOutputs] = React.useState({})
 
+  const setStates = ({ wasTriggered, scheme, path, queryParams, url }) => {
+    setLink(url)
+    setPath(path)
+    setQueryParams(queryParams)
+    setOutputs({ wasTriggered, urlFromListener: url, ...outputs })
+  };
+  const logError = error => console.log(error);
+
   React.useEffect( () => {
-    Linking.getInitialURL().then(l => {
-      setLink(l)
-      console.log("Todo bien", l)
-      const {path, queryParams} = getParamsUrl(l)
-      setPath(path)
-      setQueryParams(queryParams)
-    }).catch(error => console.log(error))
+    dlTrigger.getInitialURL().then(setStates).catch(logError);
 
     if (!__DEV__) {
-      // Linking.removeAllListeners("url");
-      Linking.addEventListener('url', ( {url} ) => {
-        console.log(`url: ${url}`)
-        const {path, queryParams} = getParamsUrl(url)
-        setLink(url)
-        setPath(path)
-        setQueryParams(queryParams)
-        setOutputs({ urlFromListener: url, ...outputs })
-      });
-
+      dlTrigger.getEventURL().then(setStates).catch(logError);
     }
   }, [])
 
-
-  // alert(dataRecived.queryParams)
   
   return (
     <View style={styles.container}>
